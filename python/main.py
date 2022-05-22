1aeeb1112738b3844c691ac3b178ecdd4b25ddeb
SHREYA GUPTA <shreyagupta240501@gmail.com>
2022-05-23 01:25:57 +0530

    Step1-step3 completed

--- python/main.py
+++ python/main.py
@@ -4,11 +4,12 @@ import pathlib
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+import json
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
 logger.level = logging.INFO
-images = pathlib.Path(__file__).parent.resolve() / "image"
+images = pathlib.Path(__file__).parent.resolve() / "images"
 origins = [ os.environ.get('FRONT_URL', 'http://localhost:3000') ]
 app.add_middleware(
     CORSMiddleware,
@@ -23,16 +24,24 @@ def root():
     return {"message": "Hello, world!"}
 
 @app.post("/items")
-def add_item(name: str = Form(...)):
-    logger.info(f"Receive item: {name}")
+def add_item(name: str = Form(...), category: str = Form(...)):
+    item = {"name": name, "category": category }
+    with open("items.json", "w") as f:
+        json.dump({"items": item}, f)
     return {"message": f"item received: {name}"}
 
-@app.get("/image/{items_image}")
-async def get_image(items_image):
-    # Create image path
-    image = images / items_image
+@app.get("/items")
+async def read_items():
+    with open("items.json", 'r') as f:
+        data = json.load(f)
+    return data
 
-    if not items_image.endswith(".jpg"):
+@app.get("/image/{image_filename}")
+async def get_image(image_filename):
+    # Create image path
+    image = images / image_filename
+
+    if not image_filename.endswith(".jpg"):
         raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
 
     if not image.exists():
